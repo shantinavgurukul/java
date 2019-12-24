44401ccc4c5d89ac2a7585e5331dc3696f17b5b0
shantinavgurukul <you@example.com>
2019-12-24 11:35:57 +0530

    first commit

---
+++ ExamplCheck.java
@@ -0,0 +1,41 @@
+package testvariable;
+
+public class ExamplCheck {
+//	static int a;
+//	public static void shanti() {
+//		a=10;
+//		System.out.println(a);
+//	}
+//	public static void vandana() {
+//		a=19;
+//		System.out.println(a);
+//	}
+//	public static void main(String[] args) {
+//		shanti();
+//		vandana();
+//		System.out.println("sunil");
+//		
+//	}
+//
+//
+//}
+	static int a;
+
+	public static int durga() {
+		a = 90;
+		return a;
+		
+	}
+
+	public static int rinki() {
+		a = 80;
+		return a;
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println(durga());
+		System.out.println(rinki());
+
+	}
+}
